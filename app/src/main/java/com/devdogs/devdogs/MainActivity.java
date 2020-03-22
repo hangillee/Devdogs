068da66a68752b8200eabb51b2b5d276e9307358
Uncamino <skfcb10@naver.com>
2020-03-23 06:02:50 +0900

    Make intro page

---
+++ app/src/main/java/com/devdogs/devdogs/MainActivity.java
@@ -0,0 +1,25 @@
+package com.devdogs.devdogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        if(getSupportActionBar() != null){
+            this.getSupportActionBar().hide();
+        }
+    }
+}
